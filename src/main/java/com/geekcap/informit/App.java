b537837bd5b6134dd12afbee10b20fd8bd6ea19f
kanwu <kanwu@cp-1.ssd-test.spark-pg0.wisc.cloudlab.us>
2017-01-30 19:39:13 -0700

    v0.2 being able to find the shortestpath

--- src/main/java/com/geekcap/informit/App.java
+++ src/main/java/com/geekcap/informit/App.java
@@ -1,6 +1,7 @@
 package com.geekcap.informit;
 
 import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.types.Path;
 
 import static org.neo4j.driver.v1.Values.parameters;
 /**
@@ -16,16 +17,25 @@ public class App
 		Session session = driver.session();
 
 		// run something
-		session.run( "CREATE (a:Person {name: {name}, title: {title}})", parameters( "name", "Arthur", "title", "King" ) );
-
-		StatementResult result = session.run( "MATCH (a:Person) WHERE a.name = {name} " + "RETURN a.name AS name, a.title AS title", parameters( "name", "Arthur" ) );
-
+		//session.run( "CREATE (a:Person {name: {name}, title: {title}})", parameters( "name", "Arthur", "title", "King" ) );
+		
+		//StatementResult result = session.run( "MATCH (a:Person) WHERE a.name = {name} " + "RETURN a.name AS name, a.title AS title", parameters( "name", "Arthur" ) );
+		StatementResult result = session.run(
+		"MATCH (n1:node), (n2:node), p = shortestPath((n1)-[*]->(n2))"
+		+ "WHERE n1.id='1' AND n2.id='4'"
+ 		+ "RETURN p",
+		parameters());
+		
+		//Path path = result.start().get("p").asPath(); 
+		
+		//for( Path.Segment segment : path ) {
+		//	System.out.println(segment.start().id(), segment.end().id());
+		//}
 		while ( result.hasNext() )
 		{
     		Record record = result.next();
-    		System.out.println( record.get( "title" ).asString() + " " + record.get( "name" ).asString() );
+			System.out.println(record.get( "p" ));
 		}
-
 		session.close();
     }
 }
